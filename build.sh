f4fdd8373d018019b939cbd064c05c706919bfc1
greennarae <smk5647@gmail.com>
2025-06-28 01:23:11 +0900

    test2

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 cd ./jindam_site/
-hugo build --themesDir=./theme/ --ignoreCache
-cd ../
-
-rm -r ./docs/*
-mv ./jindam_site/public/* ./docs/
-rmdir ././jindam_site/public
+hugo build --themesDir=./theme/ --ignoreCache --destination ../docs/
 
